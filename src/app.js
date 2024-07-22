ed455b4ef5c8211f7207a57a35a35f354d9b1d58
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-07-22 18:50:52 +0200

    Js ok

--- src/app.js
+++ src/app.js
@@ -1,6 +1,44 @@
 
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = () => {
+  let numero = document.querySelector ('#numeroCarta');
+  let numeroRandom = Math.floor(Math.random() * 12) + 1;
+  if (numeroRandom === 1){
+    numeroRandom = "A"
+  }
+  if (numeroRandom === 12){
+    numeroRandom = "K"
+  }
+  if (numeroRandom === 11){
+    numeroRandom = "Q"
+  }
+  if (numeroRandom === 10){
+    numeroRandom = "J"
+  }  
+  numero.innerHTML = numeroRandom ;       
+}
+
+const randomIcono = () => {
+let icono = document.querySelector('.iconTop');
+let iconoDos = document.querySelector('.iconBottom');
+let iconoRandom = Math.floor(Math.random() * 4) + 1;
+let iconoSeleccionado;
+if (iconoRandom === 1) {
+  iconoSeleccionado = "♥";   
+} if (iconoRandom === 2) {
+  iconoSeleccionado = "♦";  
+} if (iconoRandom === 3) {
+  iconoSeleccionado = "♣";
+} if (iconoRandom === 4) {
+  iconoSeleccionado = "♠";
+}
+if (iconoSeleccionado === "♥" || iconoSeleccionado === "♦") {
+  icono.style.color = "red"; 
+  iconoDos.style.color = "red";
+}
+iconoDos.innerHTML = iconoSeleccionado;
+icono.innerHTML = iconoSeleccionado;
+}
+randomIcono();
+
+
